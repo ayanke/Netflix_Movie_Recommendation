3dab76b098945bc6fd90dc6909755b5fb2f1803a
Andrew Yanke <ayanke@utexas.edu>
2021-10-10 15:57:42 -0500

    Add package requirements

--- movielens_script.R
+++ movielens_script.R
@@ -1,3 +1,10 @@
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+if(!require(kableExtra)) install.packages("kableExtra", repos = "http://cran.us.r-project.org")
+if(!require(OneR)) install.packages("OneR", repos = "http://cran.us.r-project.org")
+
 library(tidyverse)
 library(dplyr)
 library(caret)
